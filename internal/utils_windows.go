f24806e4c107951e61527ba495be7837641a83f1
Adm0 <12154400+Adm0@users.noreply.github.com>
2025-10-27 14:55:39 +0500

    nativetun: win: Using MD5 hash of TUN name as GUID

--- internal/utils_windows.go
+++ internal/utils_windows.go
@@ -3,8 +3,12 @@
 package internal
 
 import (
+	"crypto/md5"
 	"net"
 	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
 )
 
 func AddIpAddress(luid uint64, ip net.IP) error {
@@ -26,4 +30,16 @@ func SetMTU(luid uint64, family int, mtu int) error {
 	}
 	entry.NlMtu = uint32(mtu)
 	return SetIpInterfaceEntry(entry)
+}
+
+// NameToGuid convert TUN device name to GUID
+//
+// Parameters:
+//   - name: string - The interface name.
+//
+// Returns:
+//   - GUID: MD5 hash of name as GUID.
+func NameToGuid(name string) *windows.GUID {
+	sum := md5.Sum([]byte(name))
+	return (*windows.GUID)(unsafe.Pointer(&sum))
 }
